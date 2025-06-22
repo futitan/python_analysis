c28369cb87824090ec77157f97db2d56612ab8d4
ssuyuan <titanfu0907@gmail.com>
2025-06-22 14:49:15 +0800

    modify

--- web/app.py
+++ web/app.py
@@ -1,12 +1,48 @@
 #flask  https://flask.palletsprojects.com/en/stable/
 #Flask 和 Django 都是 Python 常用的 Web 框架  https://www.perplexity.ai/search/python-min-ru-he-shi-yong-qing-IKspVcELRvGQd6hECJaqjQ8
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+from google import genai
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+client = genai.configure(api_key=os.environ['Gemini_API_KEY'])
+
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 @app.route("/")
-def hello_world():
-    return "<H1>Hello, World!帥哥~~~</H1>"
+@app.route("/<string:question>")
+def index(question:str=""):
+    response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=f"{question},回應請輸出成為html格式")
+    html_format = response.text
+    html_format = html_format.replace("```html","").replace("```","")
+    return html_format
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    #response = model.generate_content(event.message.text)
+    response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=event.message.text
+    )
+    message = TextSendMessage(text=response.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
 #flask --app app run  (第2個APP 使檔名) 修改時要關閉再啟動
 #flask --app app run --debug  修改時不需關閉再啟動，自己會關閉重啟
